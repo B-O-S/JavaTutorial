c618c281fbc4c7aafa22925f72d9c7088518449c
B_O_S <b_o_s@ukr.net>
2018-09-30 21:14:09 +0300

    Lesson 21

---
+++ src/Lesson21_Synchronized/Main.java
@@ -0,0 +1,29 @@
+package Lesson21_Synchronized;
+
+public class Main {
+    private static int number;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Worker());
+        Thread t2 = new Thread(new Worker());
+        t1.start();
+        t2.start();
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(number);
+    }
+
+    static synchronized void increaseNumber() {
+        number++;
+    }
+
+}
