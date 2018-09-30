fa25849f5d678bca9418420879aa26edfff47ee4
B_O_S <b_o_s@ukr.net>
2018-09-30 23:16:42 +0300

    Lesson 22

---
+++ src/Lesson22_Synchronized_Code_Block/Main1.java
@@ -0,0 +1,8 @@
+package Lesson22_Synchronized_Code_Block;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Worker1 worker1 = new Worker1();
+        worker1.start();
+    }
+}
---
+++ src/Lesson22_Synchronized_Code_Block/Worker1.java
@@ -0,0 +1,65 @@
+package Lesson22_Synchronized_Code_Block;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class Worker1 {
+    private ArrayList<Integer> list1 = new ArrayList<>();
+    private ArrayList<Integer> list2 = new ArrayList<>();
+    private Random random = new Random();
+
+    private synchronized void partOne() {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list1.add(random.nextInt(100));
+    }
+    private /*synchronized*/ void partTwo() {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list2.add(random.nextInt(100));
+    }
+    private void proceed() {
+        for (int i=0; i<1000; i++) {
+            partOne();
+            partTwo();
+        }
+    }
+    void start() {
+        System.out.println("Починаємо…");
+        long startTime = System.currentTimeMillis();
+//        Thread t1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                proceed();
+//            }
+//        });
+//        Thread t2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                proceed();
+//            }
+//        });
+//        Thread t1 = new Thread(() -> proceed());
+//        Thread t2 = new Thread(() -> proceed());
+        Thread t1 = new Thread(this::proceed);
+        Thread t2 = new Thread(this::proceed);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Витрачений час: " + (endTime - startTime) + "\n"
+                + "Список 1: " + list1.size() + "\n"
+                + "Список 2: " + list2.size() + "\n");
+    }
+}
